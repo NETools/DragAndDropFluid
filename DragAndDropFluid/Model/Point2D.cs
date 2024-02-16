f5e345a60e1853cca1c8d1bc397d3d99191efb50
NETools <117414781+NETools@users.noreply.github.com>
2024-02-16 04:30:51 +0100

    Projektdateien hinzufügen.

---
+++ DragAndDropFluid/Model/Point2D.cs
@@ -0,0 +1,13 @@
+﻿namespace DragAndDropFluid.Model
+{
+	public class Point2d(float x, float y)
+	{
+		public float X { get; set; } = x;
+		public float Y { get; set; } = y;
+
+		public override string ToString()
+		{
+			return $"X: {X}; Y: {Y}";
+		}
+	}
+}
