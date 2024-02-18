9695c22eef239b54115b5d428c7c5859c244271c
NETools <117414781+NETools@users.noreply.github.com>
2024-02-18 05:30:26 +0100

    Version 1.0

---
+++ DragAndDropFluid/Misc/Toolkit.cs
@@ -0,0 +1,34 @@
+﻿using DragAndDropFluid.Model;
+using System;
+using System.Drawing;
+
+namespace DragAndDropFluid.Misc
+{
+	public static class Toolkit
+	{
+		public static (int Column, int Row) CalculateIndices(Point2d position, Point2d size, double scale)
+		{
+			double gridWidth = size.X * scale;
+			double gridHeight = size.Y * scale;
+
+			var column = (int)((position.X + gridWidth) / size.X);
+			var row = (int)((position.Y + gridHeight) / size.Y);
+
+			return (column, row);
+		}
+
+		public static (double X, double Y) CalculateCoordinates((int Column, int Row) indices, Point2d size)
+		{
+			var x = indices.Column * size.X;
+			var y = indices.Row * size.Y;
+
+			return (x, y);
+		}
+
+		public static RectangleF CreateRectangle(Point2d leftTop, Point2d size)
+		{
+			return new RectangleF((float)leftTop.X, (float)leftTop.Y, (float)size.X, (float)size.Y);
+		}
+
+	}
+}
--- DragAndDropFluid/Model/Point2D.cs
+++ DragAndDropFluid/Model/Point2D.cs
@@ -1,9 +1,9 @@
 ﻿namespace DragAndDropFluid.Model
 {
-	public class Point2d(float x, float y)
+	public class Point2d(double x, double y)
 	{
-		public float X { get; set; } = x;
-		public float Y { get; set; } = y;
+		public double X { get; set; } = x;
+		public double Y { get; set; } = y;
 
 		public override string ToString()
 		{
